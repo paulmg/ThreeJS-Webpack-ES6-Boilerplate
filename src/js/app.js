a4f88fee78e486b120e030cc0b84608c9c5e24ca
Paul Graffam <pgraffam@bloomberg.net>
2019-01-29 16:55:05 -0500

    Import styles into project

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,9 @@ import Config from './data/config';
 import Detector from './utils/detector';
 import Main from './app/main';
 
+// Styles
+import './../css/app.scss';
+
 // Check environment and set the Config helper
 if(__ENV__ === 'dev') {
   console.log('----- RUNNING IN DEV ENVIRONMENT! -----');
