8303fd1810cbae3eaa417b42441a9086809bca9b
Paul Graffam <paul.graffam@isobar.com>
2020-07-06 17:23:59 -0400

    Added vertex helper, fixed up mesh helper

--- src/js/app/helpers/meshHelper.js
+++ src/js/app/helpers/meshHelper.js
@@ -1,24 +1,41 @@
 import * as THREE from 'three';
+import { VertexNormalsHelper } from './vertexNormalsHelper';
 
 // Simple mesh helper that shows edges, wireframes, and face and vertex normals
 export default class MeshHelper {
   constructor(scene, mesh) {
-    const wireframe = new THREE.WireframeGeometry(mesh.geometry);
-    const wireLine = new THREE.LineSegments(wireframe);
-    wireLine.material.depthTest = false;
-    wireLine.material.opacity = 0.25;
-    wireLine.material.transparent = true;
-    mesh.add(wireLine);
+    this.mesh = mesh;
+    this.scene = scene;
 
-    const edges = new THREE.EdgesGeometry(mesh.geometry);
-    const edgesLine = new THREE.LineSegments(edges);
-    edgesLine.material.depthTest = false;
-    edgesLine.material.opacity = 0.25;
-    edgesLine.material.transparent = true;
-    mesh.add(edgesLine);
+    const wireframe = new THREE.WireframeGeometry(this.mesh.geometry);
+    this.wireLine = new THREE.LineSegments(wireframe);
+    this.wireLine.material.depthTest = false;
+    this.wireLine.material.opacity = 0.25;
+    this.wireLine.material.transparent = true;
 
-    scene.add(new THREE.BoxHelper(mesh));
-    scene.add(new THREE.FaceNormalsHelper(mesh, 2));
-    scene.add(new THREE.VertexNormalsHelper(mesh, 2));
+    const edges = new THREE.EdgesGeometry(this.mesh.geometry);
+    this.edgesLine = new THREE.LineSegments(edges);
+    this.edgesLine.material.depthTest = false;
+    this.edgesLine.material.opacity = 0.25;
+    this.edgesLine.material.transparent = true;
+
+    this.vertexHelper = new VertexNormalsHelper(this.mesh, 2);
+    this.boxHelper = new THREE.BoxHelper(this.mesh);
+  }
+
+  enable() {
+    this.mesh.add(this.wireLine);
+    this.mesh.add(this.edgesLine);
+
+    this.scene.add(this.vertexHelper);
+    this.scene.add(this.boxHelper);
+  }
+
+  disable() {
+    this.mesh.remove(this.wireLine);
+    this.mesh.remove(this.edgesLine);
+
+    this.scene.remove(this.vertexHelper);
+    this.scene.remove(this.boxHelper);
   }
 }
